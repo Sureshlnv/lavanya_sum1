e9a9bf8bb2102a94078b712abd749336291aa43f
JaswanthAbbineni <jaswanth.abbineni@gmail.com>
2018-07-03 16:40:41 +0530

    lavanya first comitt

---
+++ Lavanya_sum.java
@@ -0,0 +1,14 @@
+package lavanya_sum;
+
+public class Lavanya_sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=10,b=35;
+		int sum=a+b;
+		System.out.println(sum);
+	}
+	
+
+}
